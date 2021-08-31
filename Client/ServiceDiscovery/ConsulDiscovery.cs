f377341b3448c127685d3914d6027cef68c40600
deeyi2000 <deeyi2000@gmail.com>
2021-08-31 11:33:11 +0800

    feat: add ConsulDiscovery

--- Client/ServiceDiscovery/ConsulDiscovery.cs
+++ Client/ServiceDiscovery/ConsulDiscovery.cs
@@ -1,31 +1,66 @@
-﻿using System;
+﻿using Consul;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace rpcx.net.Client.ServiceDiscovery
 {
     public class ConsulDiscovery : IServiceDiscovery
     {
+        protected string _basePath;
+        protected IConsulClient _client;
+        protected CancellationTokenSource _cts;
+        protected Func<KeyValuePair<string, string>, bool> _filter;
+        protected List<KeyValuePair<string, string>> _services;
+
         public event WatcherHandler ServiceWatcher;
 
-        public IServiceDiscovery Clone(string servicePath)
+        public ConsulDiscovery(string basePath, string servicePath, ConsulClientConfiguration config) :
+            this($"{basePath}/{servicePath}", new ConsulClient(config)) { }
+
+        public ConsulDiscovery(string basePath, IConsulClient client)
         {
-            throw new NotImplementedException();
+            _basePath = basePath.Trim('/');
+            _client = client;
+            _cts = new CancellationTokenSource();
+
+            Watch();
         }
 
-        public void Close()
+        private void Watch()
         {
-            throw new NotImplementedException();
+            var token = _cts.Token;
+            var opt = new QueryOptions()
+            {
+                WaitTime = TimeSpan.FromSeconds(15)
+            };
+            var prefix = $"{_basePath}/";
+            Task.Run(async () =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    var resp = await _client.KV.List(_basePath, opt, token);
+                    if (resp is null || resp.LastIndex == opt.WaitIndex)
+                        continue;
+                    opt.WaitIndex = resp.LastIndex;
+
+                    var ss = resp.Response.Where(kv => kv.Key.StartsWith(prefix))
+                                            .Select(kv => new KeyValuePair<string, string>(kv.Key.Substring(prefix.Length), Encoding.UTF8.GetString(kv.Value)));
+                    ss = _filter is null ? ss : ss.Where(_filter);
+                    var svcs = ss.ToList();
+
+                    Interlocked.Exchange(ref _services, svcs);
+                    ServiceWatcher?.Invoke(this, svcs);
+                }
+            }, token);
         }
 
-        public List<KeyValuePair<string, string>> GetServices()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SetFilter(Func<KeyValuePair<string, string>, bool> filter)
-        {
-            throw new NotImplementedException();
-        }
+        public IServiceDiscovery Clone(string servicePath) => new ConsulDiscovery($"{_basePath}/{servicePath}", _client);
+        public List<KeyValuePair<string, string>> GetServices() => _services;
+        public void SetFilter(Func<KeyValuePair<string, string>, bool> filter) => Interlocked.Exchange(ref _filter, filter);
+        public void Close() => _cts.Cancel();
     }
 }
