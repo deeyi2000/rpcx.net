9676490449fc7e031970aa845f0b393bc2bf7e17
13981712066 <piyongcai@163.com>
2021-10-11 16:55:00 +0800

    增加Google.Protobuf序列化支持

--- Shared/Codecs/Serializer/ProtobufSerializer.cs
+++ Shared/Codecs/Serializer/ProtobufSerializer.cs
@@ -1,27 +1,40 @@
-﻿using System;
+﻿using Google.Protobuf;
+using System;
+using System.IO;
 
-namespace rpcx.net.Shared.Codecs.Serializer
-{
-    public class ProtobufSerializer : ISerializer
-    {
+namespace rpcx.net.Shared.Codecs.Serializer {
+    public class ProtobufSerializer : ISerializer {
         private static ProtobufSerializer _default;
-        public static ProtobufSerializer Default
-        {
-            get
-            {
+        public static ProtobufSerializer Default {
+            get {
                 if (_default is null)
                     _default = new ProtobufSerializer();
                 return _default;
             }
         }
 
-        public object Deserialize(Type type, ReadOnlyMemory<byte> bytes)
-        {
+        public object Deserialize(Type type, ReadOnlyMemory<byte> bytes) {
+            if (type.IsAssignableFrom(typeof(IMessage))) {
+                var msg = (IMessage)Activator.CreateInstance(type);
+
+                using (var ms = new MemoryStream())
+                using (var input = new CodedInputStream(ms)) {
+                    msg.MergeFrom(input);
+                }
+
+                return msg;
+            }
             throw new NotImplementedException();
         }
 
-        public byte[] Serialize(object value)
-        {
+        public byte[] Serialize(object value) {
+            if (value is IMessage msg) {
+                using (var ms = new MemoryStream())
+                using (var output = new CodedOutputStream(ms)) {
+                    msg.WriteTo(output);
+                    return ms.ToArray();
+                }
+            }
             throw new NotImplementedException();
         }
     }
